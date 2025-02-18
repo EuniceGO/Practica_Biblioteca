bf204b437d366efd0fe2c07c0720c5e6091a86f3
Katherine Gutierrez <katherine.gutierrez@catolica.edu.sv>
2025-02-17 23:06:51 -0600

    Consultas LINQ

--- Practica_Biblioteca/Controllers/LibrosController.cs
+++ Practica_Biblioteca/Controllers/LibrosController.cs
@@ -168,6 +168,140 @@ namespace Practica_Biblioteca.Controllers
             return Ok(libros);
         }
 
+        [HttpGet]
+        [Route("LibroById")]
+        public IActionResult LibrosMayores()
+        {
+            var listadoLibros = (from l in _bibliotecaContexto.libro
+                                 where l.AñoPublicacion > 2000
+                                 select l).ToList();
 
+            if (listadoLibros == null)
+            {
+                return NotFound();
+            }
+            return Ok(listadoLibros);
+
+        }
+
+
+        [HttpGet]
+        [Route("ContarLibroById")]
+        public IActionResult Contar(int id)
+        {
+            var listadoLibros = (from l in _bibliotecaContexto.libro
+                                 where l.autor_id == id
+                                 select l).Count();
+
+            if (listadoLibros == 0)
+            {
+                return NotFound();
+            }
+            return Ok(listadoLibros);
+
+        }
+
+        [HttpGet]
+        [Route("GetTituloLibro/{titulo}")]
+
+        public IActionResult GetLibroTitulo(string titulo)
+        {
+            var AutorById = (from l in _bibliotecaContexto.libro
+                             join a in _bibliotecaContexto.autor
+                             on l.autor_id equals a.Id
+                             where l.Titulo == titulo
+                             select new
+                             {
+                                 l.id,
+                                 l.Titulo,
+                                 l.AñoPublicacion,
+                                 l.autor_id,
+                                 l.categoria_id,
+                                 l.Resumen,
+                                 a.Nombre
+
+                             }).ToList();
+
+            if (AutorById.Count() == 0)
+            {
+                return NotFound();
+            }
+            return Ok(AutorById);
+        }
+
+        /// Segundas consultas LINQ
+        /// 
+        [HttpGet]
+        [Route("Recientes")]
+        public IActionResult LibrosMasRecientes()
+        {
+            var librosMasRecientes = (from l in _bibliotecaContexto.libro
+                                      orderby l.AñoPublicacion descending
+                                      select l)
+                                      .ToList();
+
+            return Ok(librosMasRecientes);
+        }
+
+        [HttpGet]
+        [Route("CantidadLibros")]
+        public IActionResult CantidadLibrosPorAño()
+        {
+            var cantidadLibrosPorAño = (from l in _bibliotecaContexto.libro
+                                        group l by l.AñoPublicacion into g
+                                        select new
+                                        {
+                                            Año = g.Key,
+                                            CantidadLibros = g.Count()
+                                        }).ToList();
+
+            return Ok(cantidadLibrosPorAño);
+        }
+
+        [HttpGet]
+        [Route("VerAutorLibros/{id}")]
+        public IActionResult VerificarAutorLibros(int id)
+        {
+            var autorConLibros = (from l in _bibliotecaContexto.libro
+                                  where l.AutorId == id
+                                  select new
+                                  {
+                                      l.AutorId
+                                  }).FirstOrDefault();
+
+            if (autorConLibros == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(autorConLibros);
+        }
+
+    
+
+        [HttpGet]
+        [Route("PrimerLibroByAutor/{id}")]
+        public IActionResult PrimerLibro(int id)
+        {
+            var listaLibro = (from l in _bibliotecaContexto.libro
+                              where l.autor_id == id
+                              orderby l.AñoPublicacion ascending
+                              select new
+                              {
+                                  l.id,
+                                  l.Titulo,
+                                  l.AñoPublicacion,
+                                  l.autor_id,
+                                  l.categoria_id,
+                                  l.Resumen
+                              }).FirstOrDefault();
+
+            if (listaLibro == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(listaLibro);
+        }
     }
 }
